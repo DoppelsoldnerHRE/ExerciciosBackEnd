a80eb58382489d13c82ba829e9f2a47fe75d9c4a
yuri.298323 <yuri.298323@alunosatc.edu.br>
2025-08-14 21:26:17 -0300

    Continuação da tarefa

---
+++ src/Pergunta2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Pergunta2 {
+    public static void main(String[] args) {
+        funcionario();
+    }
+
+    static void funcionario() {
+        String nome= "julia";
+        String cargo="artist";
+        int salario= 0;
+
+
+        System.out.println(nome);
+        System.out.println(cargo);
+        System.out.println(salario);
+
+    }
+}
---
+++ src/Pergunta3.java
@@ -0,0 +1,15 @@
+public class Pergunta3 {
+    public static void main(String[]args){
+
+
+    }
+    static void compra(){
+        String nome = "Feijao";
+        Double valor = 5.0;
+        Boolean dispnibilidade = true;
+
+
+
+
+    }
+}
